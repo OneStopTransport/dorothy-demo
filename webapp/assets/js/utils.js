9cdb5d96c2d90540099557c1985d8dc666edc43f
Ricardo Vitorino <rvitorino@ipn.pt>
2015-01-16 11:25:31 +0000

    Adds new features and cleans code:
    - new function to save vehicle attributes;
    - adds sidebar for routing instructions;
    - removes useless variables;
    - adds some comments to help understanding;

--- webapp/assets/js/utils.js
+++ webapp/assets/js/utils.js
@@ -1,4 +1,3 @@
-var lisbonCentre = "38.7436266,-9.1602037";
 $(document).ready(function() {
   map.locate();
   updateVehicleProps($('#vehicle-choice').find(".selection").text());
@@ -25,6 +24,9 @@ var vehicleProps = {
     'HGV + Trailer': [15, 3, 2.5, 10],
 };
 
+// Default vehicle is a Goods Vehicle
+var currentVehicle = vehicleProps['Goods'].join();
+
 // Function to change value of input
 $.fn.changeVal = function (v) {
     return $(this).val(v).trigger("change");
@@ -37,19 +39,37 @@ $(".dropdown-menu li a").click(function() {
   $(this).parents(".btn-group").find('.selection').text(option);
   $(this).parents(".btn-group").find('.selection').val(option);
 });
+
+// By choosing a vehicle type in the form, fill it with
+// values from the dictionary vehicleProps
 function updateVehicleProps(option) {
-    // Updates the image
-    var attrs = vehicleProps[option];
-    var choice = option.indexOf("HGV") >= 0 ? "assets/img/vehicles/HGV.png" : "assets/img/vehicles/Goods.png";
-    $("#vehicle-type-img")[0].src = choice;
-    $("#weight-input").changeVal(attrs[0]);
-    $("#height-input").changeVal(attrs[1]);
-    $("#width-input").changeVal(attrs[2]);
-    $("#length-input").changeVal(attrs[3]);
+  // Updates the image in modal window
+  var attrs = vehicleProps[option];
+  var choice = option.indexOf("HGV") >= 0 ? "assets/img/vehicles/HGV.png" : "assets/img/vehicles/Goods.png";
+  // Updates the attributes in the form
+  $("#vehicle-type-img")[0].src = choice;
+  $("#weight-input").changeVal(attrs[0]);
+  $("#height-input").changeVal(attrs[1]);
+  $("#width-input").changeVal(attrs[2]);
+  $("#length-input").changeVal(attrs[3]);
 };
 
+// Updates the string to be sent to Routino API
+// with user-defined values in the vehicle form
+function saveVehicleProps() {
+  var vehicleChoice = [
+      $("#weight-input").val(),
+      $("#height-input").val(),
+      $("#width-input").val(),
+      $("#length-input").val(),
+  ];
+  currentVehicle = vehicleChoice.join();
+};
 
+// Sets the correct style to buttons added to
+// the map (location, vehicle and route)
 function fixButtons() {
+  // Buttons size
   var buttons = document.getElementsByClassName('leaflet-control');
   for (var index in buttons) {
     if (buttons[index].children) {
@@ -58,7 +78,7 @@ function fixButtons() {
       buttons[index].children[0].style['line-height'] = 'inherit';
     }
   };
-  
+  // Fontawesome icons
   var icons = document.getElementsByClassName('button-icon');
   for (var index in icons) {
     if (icons[index].children) {
