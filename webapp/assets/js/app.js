0662d3f524039f57de330667e4425cb754bfbed1
Ricardo Vitorino <rvitorino@ipn.pt>
2015-02-02 10:38:40 +0000

    Removes unused code from app.js

--- webapp/assets/js/app.js
+++ webapp/assets/js/app.js
@@ -1,4 +1,3 @@
-var map, featureList;
 var userLocation;
 
 $("#sidebar-hide-btn").click(function() {
@@ -40,72 +39,12 @@ var markerClusters = new L.MarkerClusterGroup({
   disableClusteringAtZoom: 16
 });
 
-/* Empty layer placeholder to add to layer control for listening when to add/remove museums to markerClusters layer */
-// var museumLayer = L.geoJson(null);
-// var museums = L.geoJson(null, {
-//   pointToLayer: function (feature, latlng) {
-//     return L.marker(latlng, {
-//       icon: L.icon({
-//         iconUrl: "assets/img/museum.png",
-//         iconSize: [24, 28],
-//         iconAnchor: [12, 28],
-//         popupAnchor: [0, -25]
-//       }),
-//       title: feature.properties.NAME,
-//       riseOnHover: true
-//     });
-//   },
-//   onEachFeature: function (feature, layer) {
-//     if (feature.properties) {
-//       var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.NAME + "</td></tr>" + "<tr><th>Phone</th><td>" + feature.properties.TEL + "</td></tr>" + "<tr><th>Address</th><td>" + feature.properties.ADRESS1 + "</td></tr>" + "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + "</a></td></tr>" + "<table>";
-//       layer.on({
-//         click: function (e) {
-//           $("#feature-title").html(feature.properties.NAME);
-//           $("#feature-info").html(content);
-//           $("#featureModal").modal("show");
-//           highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-//             stroke: false,
-//             fillColor: "#00FFFF",
-//             fillOpacity: 0.7,
-//             radius: 10
-//           }));
-//         }
-//       });
-//       $("#feature-list tbody").append('<tr class="feature-row" id="'+L.stamp(layer)+'"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/museum.png"></td><td class="feature-name">'+layer.feature.properties.NAME+'</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
-//       museumSearch.push({
-//         name: layer.feature.properties.NAME,
-//         address: layer.feature.properties.ADRESS1,
-//         source: "Museums",
-//         id: L.stamp(layer),
-//         lat: layer.feature.geometry.coordinates[1],
-//         lng: layer.feature.geometry.coordinates[0]
-//       });
-//     }
-//   }
-// });
-// $.getJSON("data/DOITT_MUSEUM_01_13SEPT2010.geojson", function (data) {
-//   museums.addData(data);
-// });
-
 map = L.map("map", {
   zoom: 13,
   center: [38.7436266,-9.1602037],
   layers: [mapquestOSM, markerClusters],
   zoomControl: false,
   attributionControl: false
-});
-
-/* Layer control listeners that allow for a single markerClusters layer */
-map.on("overlayadd", function(e) {
-  if (e.layer === museumLayer) {
-    markerClusters.addLayer(museums);
-  }
-});
-
-map.on("overlayremove", function(e) {
-  if (e.layer === museumLayer) {
-    markerClusters.removeLayer(museums);
-  }
 });
 
 /* GPS enabled geolocation control set to follow the user's location */
@@ -155,23 +94,7 @@ if (document.body.clientWidth <= 767) {
   var isCollapsed = false;
 }
 
-var baseLayers = {
-  // "Street Map": mapquestOSM,
-  // "Aerial Imagery": mapquestOAM,
-  // "Imagery with Streets": mapquestHYB
-};
+var baseLayers = {};
 
-var groupedOverlays = {
-  // "Points of Interest": {
-  //   "<img src='assets/img/theater.png' width='24' height='28'>&nbsp;Theaters": theaterLayer,
-  //   "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer
-  // },
-  // "Reference": {
-  //   "Boroughs": boroughs,
-  //   "Subway Lines": subwayLines
-  // }
-};
+var groupedOverlays = {};
 
-// var layerControl = L.control.groupedLayers(baseLayers, groupedOverlays, {
-//   collapsed: isCollapsed
-// }).addTo(map);
